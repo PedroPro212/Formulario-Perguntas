94544536c47f5ed054b6678c7069851be1c688c3
Lucas <lucasgrossicostick@gmail.com>
2022-09-06 22:11:48 -0300

    Finalizaçao do "exibir professores", inicio do "cadastrar professores"

--- Avaliacao/Avaliacao/Admin/Professor/ExibirProfessores.aspx.designer.cs
+++ Avaliacao/Avaliacao/Admin/Professor/ExibirProfessores.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace Avaliacao.Admin.Professor {
     public partial class ExibirProfessores {
         
         /// <summary>
-        /// Controle txtNome.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtNome;
-        
-        /// <summary>
         /// Controle Image1.
         /// </summary>
         /// <remarks>
@@ -29,6 +20,15 @@ namespace Avaliacao.Admin.Professor {
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Image Image1;
+        
+        /// <summary>
+        /// Controle txtNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNome;
         
         /// <summary>
         /// Controle PesquisarProf.
