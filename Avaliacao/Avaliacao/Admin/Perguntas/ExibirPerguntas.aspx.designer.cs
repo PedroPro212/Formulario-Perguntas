0194df9e98612c2c46250c2a8db7dae3cb6aef30
Pedro Oliveira <pedrogamer212004@gmail.com>
2022-09-14 20:13:05 -0300

    Alteração

--- Avaliacao/Avaliacao/Admin/Perguntas/ExibirPerguntas.aspx.designer.cs
+++ Avaliacao/Avaliacao/Admin/Perguntas/ExibirPerguntas.aspx.designer.cs
@@ -1,50 +1,52 @@
 ﻿//------------------------------------------------------------------------------
 // <gerado automaticamente>
-//     Este código foi gerado por uma ferramenta.
+//     Esse código foi gerado por uma ferramenta.
 //
 //     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
 //     o código for recriado
 // </gerado automaticamente>
 //------------------------------------------------------------------------------
 
-namespace Avaliacao.Admin.Perguntas {
-    
-    
-    public partial class WebForm1 {
-        
+namespace Avaliacao.Admin.Perguntas
+{
+
+
+    public partial class WebForm1
+    {
+
         /// <summary>
         /// Controle txtPergunta.
         /// </summary>
         /// <remarks>
         /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtPergunta;
-        
+
         /// <summary>
         /// Controle PesquisarPergunta.
         /// </summary>
         /// <remarks>
         /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button PesquisarPergunta;
-        
+
         /// <summary>
         /// Controle CadastrarPergunta.
         /// </summary>
         /// <remarks>
         /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button CadastrarPergunta;
-        
+
         /// <summary>
         /// Controle grdPerguntas.
         /// </summary>
         /// <remarks>
         /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView grdPerguntas;
     }
